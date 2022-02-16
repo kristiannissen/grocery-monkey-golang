6bf9ed256cf6811ac5506d032dbd6af07450e005
Kristian Nissen <kristian.nissen@gmail.com>
2022-02-16 12:15:21 +0100

    Initial go

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi Kitty")
+}
+
+func main() {
+	var port string = os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
